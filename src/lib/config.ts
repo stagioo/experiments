bdd334a481a3206c6ebd13acb829c18fd26ada6b
code-env <org.codeen@gmail.com>
2025-07-03 02:10:01 +0100

    feat: add configuration file for mediasoup setup with WebRTC parameters

---
+++ src/lib/config.ts
@@ -0,0 +1,56 @@
+import {
+  RtpCodecCapability,
+  TransportListenInfo,
+  WorkerLogTag,
+} from "mediasoup/node/lib/types";
+import os from "os";
+
+export const config = {
+  listenIp: "0.0.0.0",
+  listenPort: 3000,
+  mediasoup: {
+    numWorkers: Object.keys(os.cpus()).length,
+    workerSettings: {
+      logLevel: "debug",
+      logTags: [
+        "info",
+        "ice",
+        "dtls",
+        "rtp",
+        "srtp",
+        "rtcp",
+        "rtx",
+        "bwe",
+      ] as WorkerLogTag[],
+      rtcMinPort: 10000,
+      rtcMaxPort: 10100,
+    },
+    router: {
+      mediaCodecs: [
+        {
+          kind: "audio",
+          mimeType: "audio/opus",
+          clockRate: 48000,
+          channels: 2,
+        },
+        {
+          kind: "video",
+          mimeType: "video/VP8",
+          clockRate: 90000,
+          parameters: {
+            "x-google-start-bitrate": 1000,
+          },
+        },
+      ] as RtpCodecCapability[],
+    },
+    webRtcTransport: {
+      listenIps: [
+        {
+          ip: "0.0.0.0",
+          announcedIp: "127.0.0.1",
+        },
+      ] as TransportListenInfo[],
+      maxIncomingBitrate: 1500000,
+    },
+  },
+} as const;
